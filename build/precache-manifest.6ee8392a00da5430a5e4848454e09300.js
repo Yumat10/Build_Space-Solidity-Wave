2bdd233df388125d9c9fd9d36484df4ee1d775e9
Yumat <mr.yuma.tanaka@gmail.com>
2021-11-10 11:10:35 -0500

    (1) Installed packages with yarn instead of npm (2) Added framer-motion (3) Added CSS to cusomize layout

---
+++ build/precache-manifest.6ee8392a00da5430a5e4848454e09300.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a92164300f3f64baa36759db85a14029",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4b9fe08fcc1d4b281af2",
+    "url": "/static/css/main.3aa6bcaf.chunk.css"
+  },
+  {
+    "revision": "e018f9661af28315f4da",
+    "url": "/static/js/2.a8eef79a.chunk.js"
+  },
+  {
+    "revision": "4b9fe08fcc1d4b281af2",
+    "url": "/static/js/main.dd4c2158.chunk.js"
+  },
+  {
+    "revision": "f67c199efb9f2c29f389",
+    "url": "/static/js/runtime-main.f294effb.js"
+  }
+]);
